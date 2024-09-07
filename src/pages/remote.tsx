9e9c3634da093bdd7aaff4472d9ee0126018429a
Bilguudei Baljinnyam <bilguudei.b8@gmail.com>
2024-09-07 23:15:38 +0200

    Compilation error fixed

--- src/pages/remote.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Phase0Component from "@/components/Test/Phase0Component";
-
-const Remote: React.FC = ()  => {
-    return <div>
-        <Phase0Component />
-    </div>
-}
-export default Remote
