25c40cdc122af76bb9bca71307cced88fca57077
Bilguudei Baljinnyam <bilguudei.b8@gmail.com>
2024-09-15 03:29:27 +0200

    Update git ignore, fix types pages

--- src/pages/types/[id].tsx
+++ src/pages/types/[id].tsx
@@ -1,6 +1,6 @@
 // pages/content/[id].js
 import {TYPES} from "@/components/Test/Properties";
-import {GetStaticPaths, GetStaticProps, NextPage} from "next";
+import {GetStaticPaths, GetStaticProps, InferGetStaticPropsType} from "next";
 import Skeleton from "@/components/Skeleton";
 import markDownToHtml from "@/util/markDownToHtml";
 import TextEdgy from "@/components/TextEdgy";
@@ -10,7 +10,8 @@ interface ContentPageProps {
     markdown: string;
 }
 
-const ContentPage: NextPage<ContentPageProps> = ({id, markdown}) => {
+export default function Page({id, markdown}: InferGetStaticPropsType<typeof getStaticProps>) {
+
     return (
         <Skeleton showNavbar={true}>
             <div className={"pb-4"}>
@@ -26,11 +27,9 @@ const ContentPage: NextPage<ContentPageProps> = ({id, markdown}) => {
     );
 };
 
-export default ContentPage;
 
-
-// pages/content/[id].js
-export const getStaticPaths: GetStaticPaths = async () => {
+// pages/types/[id].js
+export const getStaticPaths = (async () => {
     // Generate an array of 14 page ids
     const typeValues = Object.values(TYPES).map((type) => type.toLowerCase());
     const paths = typeValues.map((type) => {
@@ -44,21 +43,40 @@ export const getStaticPaths: GetStaticPaths = async () => {
         paths,
         fallback: false, // Any paths not returned by getStaticPaths will result in a 404
     };
-}
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async (context) => {
+    const serverURL = "https://melogy.vercel.app"
+
+    if (!context.params) {
+        return {
+            notFound: true
+        }
+    }
+    const id = context.params.id;
+    if (typeof id !== "string") {
+        return {
+            notFound: true
+        }
+    }
+    const res = await fetch(`${serverURL}/types_markdowns/${id}.mdx`)
+
+    if (!res) {
+        return {
+            notFound: true
+        }
+    }
 
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-    const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-    const id = params?.id as string;
-
-    const res = await fetch(`${baseURL}/types_markdowns/${id}.mdx`)
     const md: string = await res.text()
-    const mdHtml = await markDownToHtml(md)
-
+    const mdHtml: string = await markDownToHtml(md)
+    console.log(md)
     return {
         props: {
             id,
             markdown: mdHtml
         },
     };
-};
+
+}) satisfies GetStaticProps<ContentPageProps>
+
