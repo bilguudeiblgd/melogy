fcc874b2b91b0a36e2b14c0864c63749f232b2a6
Bilguudei Baljinnyam <bilguudei.b8@gmail.com>
2024-09-16 22:39:40 +0200

    Add logging

--- src/components/InAppBrowserWarning.tsx
+++ src/components/InAppBrowserWarning.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import InApp from "detect-inapp";
 
 const InAppBrowserWarning = () => {
     const openInBrowser = () => {
         const url = window.location.href;
         window.open(url, '_system'); // Opens the link in the device's default browser
     };
+    useEffect(() => {
+        const inapp = new InApp(navigator.userAgent || navigator.vendor)
+        console.log(navigator.userAgent)
+        console.log("inapp in app: ", inapp.isInApp)
+    }, []);
     return (
         <div style={{padding: '10px', backgroundColor: '#f0f0f0', textAlign: 'center'}}>
             <p>You{"'"}re viewing this in the in-app browser.</p>
-            <button onClick={openInBrowser} style={{
-                padding: '10px',
-                backgroundColor: '#007bff',
-                color: '#fff',
-                border: 'none',
-                borderRadius: '5px',
-                cursor: 'pointer'
-            }}>
-                Open in Default Browser
-            </button>
+            <p>Please use actual browser.</p>
         </div>
     );
 };
