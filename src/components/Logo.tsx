0a5fd4a9b9bbcdb9c3a1bcf6e0458794fa53b5c8
Bilguudei Baljinnyam <bilguudei.b8@gmail.com>
2025-06-21 17:26:57 +0200

    change logo add google track

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
     return (
         <div className={"flex flex-row justify-center align-center"}>
             <div>
-                <Image src={'/whitelogo.png'} alt={"Logo of illustrated face with multiple layers"}  width={48} height={48} />
+                <Image src={'/yellowlogo.png'} alt={"Logo of illustrated face with multiple layers"}  width={48} height={48} />
             </div>
             <TextEdgy className={"hidden md:flex justify-center items-center ml-1 mt-1 text-4xl"}>
                 <span className={`text-accent`}>ME</span>
